7aa23e7ecabd9ef53f19b29537a4f361525f61dd
mahdibagheri <bagheri.almas@gmail.com>
2023-03-10 22:27:55 +0330

    create template and detail view for lists

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,7 @@
-from .views import todolist_list_view
+from .views import todolist_list_view,todolist_detail_view
 from django.urls import path
 
 urlpatterns = [
-    path('', todolist_list_view, name='lists')
+    path('', todolist_list_view, name='lists'),
+    path('<int:pk>/', todolist_detail_view, name='list_items')
 ]
--- lists/views.py
+++ lists/views.py
@@ -9,3 +9,14 @@ def todolist_list_view(request):
     user = request.user
     todolist = TodoList.objects.filter(user=user)
     return render(request, 'list/lists.html', {'todolist': todolist})
+
+
+@login_required()
+def todolist_detail_view(request, pk):
+    selected_list = TodoList.objects.get(pk=pk)
+    items = selected_list.items
+    context = {
+        'selected_list': selected_list,
+        'items': items.all()
+    }
+    return render(request, 'list/todolist_items.html', context)
